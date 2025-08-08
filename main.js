06e97965416c395f3a3b5aa39d9f5f388158ef13
Muzamil Ali <muzamil.ali@ili.digital>
2025-08-08 14:58:12 +0500

    Project Slider and Contact form added

--- main.js
+++ main.js
@@ -1,50 +1,26 @@
-const mobileNav = () => {
-    const rightBars = document.querySelector('.right-bars');
-    const mobileNav = document.querySelector('.mobile-nav');
-    const mobileLinks = document.querySelectorAll('.mobile-nav-link')
+import mobileNavigation from "./utility.js";
+mobileNavigation();
 
-    let isMobileNavOpen =  false;
 
-    rightBars.addEventListener( 'click', () => {
-        isMobileNavOpen = !isMobileNavOpen;
-        if (isMobileNavOpen){            
-            mobileNav.style.display = 'flex'; 
-            document.body.style.overflowY = 'hidden';
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            })
-        } else {
-            mobileNav.style.display = 'none'; 
-            document.body.style.overflowY = 'auto';
-        }
-    });
+// let currentIndex = 0;
+// const cards = document.querySelectorAll('.tile');
 
-    mobileLinks.forEach(link => {
-        link.addEventListener( 'click', () => {
-            isMobileNavOpen = false;
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        })
-    })
-}
+// function showCard(value){
+//     cards.forEach( (card, i) => {
+//         card.classList.toggle('active', i === value);
+//     } );
+// }
 
-let currentIndex = 0;
-const cards = document.querySelectorAll('.tile');
+// function nextCard() {
+//     currentIndex = (currentIndex + 1) % cards.length;
+//     showCard(currentIndex);
+// }
 
-function showCard(value){
-    cards.forEach( (card, i) => {
-        card.classList.toggle('active', i === value);
-    } );
-}
+// function prevCard() {
+//     currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+//     showCard(currentIndex);
+// }
 
-function nextCard() {
-    currentIndex = (currentIndex + 1) % cards.length;
-    showCard(currentIndex);
-}
 
-function prevCard() {
-    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-    showCard(currentIndex);
-}
+
 
