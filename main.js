d0d195b23f012cd27aa3e2ca5ac2653e64f98da8
Muzamil Ali <muzamil.ali@ili.digital>
2025-08-11 09:18:00 +0500

    Project completed

--- main.js
+++ main.js
@@ -2,24 +2,24 @@ import mobileNavigation from "./utility.js";
 mobileNavigation();
 
 
-let currentIndex = 0;
-const cards = document.querySelectorAll('.tile');
+// let currentIndex = 0;
+// const cards = document.querySelectorAll('.tile');
 
-function showCard(value){
-    cards.forEach( (card, i) => {
-        card.classList.toggle('active', i === value);
-    } );
-}
+// function showCard(value){
+//     cards.forEach( (card, i) => {
+//         card.classList.toggle('active', i === value);
+//     } );
+// }
 
-function nextCard() {
-    currentIndex = (currentIndex + 1) % cards.length;
-    showCard(currentIndex);
-}
+// function nextCard() {
+//     currentIndex = (currentIndex + 1) % cards.length;
+//     showCard(currentIndex);
+// }
 
-function prevCard() {
-    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-    showCard(currentIndex);
-}
+// function prevCard() {
+//     currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+//     showCard(currentIndex);
+// }
 
 
 
--- utility.js
+++ utility.js
@@ -9,10 +9,10 @@ const mobileNav = () => {
         isMobileNavOpen = !isMobileNavOpen;
         if (isMobileNavOpen){            
             mobileNav.style.display = 'flex'; 
-            document.body.style.overflowY = 'hidden';
             window.scrollTo({
                 top:0, behavior:'smooth'
             });
+            document.body.style.overflowY = 'hidden';
         } else {
             mobileNav.style.display = 'none'; 
             document.body.style.overflowY = 'auto';
