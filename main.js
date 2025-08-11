97ce09f3576907e1a83df4fd00d6848eb70fe1f1
Muzamil Ali <muzamil.ali@ili.digital>
2025-08-11 11:58:32 +0500

    New Changes

--- main.js
+++ main.js
@@ -1,26 +1,52 @@
-import mobileNavigation from "./utility.js";
-mobileNavigation();
+let currentIndex = 0;
+const cards = document.querySelectorAll('.tile');
 
+function showCard(value){
+    cards.forEach( (card, i) => {
+        card.classList.toggle('active', i === value);
+    } );
+}
 
-// let currentIndex = 0;
-// const cards = document.querySelectorAll('.tile');
+function nextCard() {
+    currentIndex = (currentIndex + 1) % cards.length;
+    showCard(currentIndex);
+}
 
-// function showCard(value){
-//     cards.forEach( (card, i) => {
-//         card.classList.toggle('active', i === value);
-//     } );
-// }
+function prevCard() {
+    currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+    showCard(currentIndex);
+}
 
-// function nextCard() {
-//     currentIndex = (currentIndex + 1) % cards.length;
-//     showCard(currentIndex);
-// }
+let isMobileNavOpen =  false;
+const rightBars = document.querySelector('.right-bars');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileLinks = document.querySelectorAll('.mobile-nav-link')
 
-// function prevCard() {
-//     currentIndex = (currentIndex - 1 + cards.length) % cards.length;
-//     showCard(currentIndex);
-// }
+function mobileNavigation(){
 
+    rightBars.addEventListener( 'click', () => {
+        console.log(isMobileNavOpen)
+        isMobileNavOpen = !isMobileNavOpen;
+        console.log(isMobileNavOpen)
+        if (isMobileNavOpen){            
+            mobileNav.style.display = 'flex'; 
+            window.scrollTo({
+                top:0, behavior:'smooth'
+            });
+            document.body.style.overflowY = 'hidden';
+        } else {
+            mobileNav.style.display = 'none'; 
+            document.body.style.overflowY = 'auto';
+        }
+    });
 
-
+    mobileLinks.forEach(link => {
+        link.addEventListener( 'click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        })
+    })
+}
+  
 
--- utility.js
+++ utility.js
@@ -1,31 +1,31 @@
-const mobileNav = () => {
-    const rightBars = document.querySelector('.right-bars');
-    const mobileNav = document.querySelector('.mobile-nav');
-    const mobileLinks = document.querySelectorAll('.mobile-nav-link')
+// const mobileNav = () => {
+//     const rightBars = document.querySelector('.right-bars');
+//     const mobileNav = document.querySelector('.mobile-nav');
+//     const mobileLinks = document.querySelectorAll('.mobile-nav-link')
 
-    let isMobileNavOpen =  false;
+//     let isMobileNavOpen =  false;
 
-    rightBars.addEventListener( 'click', () => {
-        isMobileNavOpen = !isMobileNavOpen;
-        if (isMobileNavOpen){            
-            mobileNav.style.display = 'flex'; 
-            window.scrollTo({
-                top:0, behavior:'smooth'
-            });
-            document.body.style.overflowY = 'hidden';
-        } else {
-            mobileNav.style.display = 'none'; 
-            document.body.style.overflowY = 'auto';
-        }
-    });
+//     rightBars.addEventListener( 'click', () => {
+//         isMobileNavOpen = !isMobileNavOpen;
+//         if (isMobileNavOpen){            
+//             mobileNav.style.display = 'flex'; 
+//             window.scrollTo({
+//                 top:0, behavior:'smooth'
+//             });
+//             document.body.style.overflowY = 'hidden';
+//         } else {
+//             mobileNav.style.display = 'none'; 
+//             document.body.style.overflowY = 'auto';
+//         }
+//     });
 
-    mobileLinks.forEach(link => {
-        link.addEventListener( 'click', () => {
-            isMobileNavOpen = false;
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        })
-    })
-}
+//     mobileLinks.forEach(link => {
+//         link.addEventListener( 'click', () => {
+//             isMobileNavOpen = false;
+//             mobileNav.style.display = 'none';
+//             document.body.style.overflowY = 'auto';
+//         })
+//     })
+// }
 
-export default mobileNav
+// export default mobileNav
